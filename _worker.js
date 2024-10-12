02abad926887ea37cb4fefc8d9e3e217fe798b86
HWM2031 <84234566+HWM2031@users.noreply.github.com>
2024-10-12 20:43:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'acd0458e-aa57-4612-9c4d-427bb5689735';
 
-let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'62.72.163.109'
+let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'128.204.223.118'
 
 let sub = '';// 避免项目被滥用，现已取消内置订阅器
 let subconverter = 'SUBAPI.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假uuid和host订阅。
